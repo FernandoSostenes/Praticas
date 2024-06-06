d1c0c5e23bb829c4d27f957adb7078fb964cb19c
FernandoSostenes <fernandosostenes@gmail.com>
2024-06-06 19:43:18 -0300

    Add files via upload
    Prova 1 da Aula 1

---
+++ Prova 1.py
@@ -0,0 +1,8 @@
+print ('Porfavor, insira as seguintes informações para o calculo da área do retângulo: \n')
+
+base_retangulo = float(input('Valor da base do retângulo(em centimetros): '))
+altura_retangulo = float(input('Valor da altura do retângulo(em centimetros): '))
+
+area_retangulo = base_retangulo * altura_retangulo
+
+print(f'Área do retângulo é de {area_retangulo} cm².\n')
